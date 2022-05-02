99a283ad83081faee02c22d4b96ee317c215d497
SanyFaysal <tfoysalahmed@gmail.com>
2022-05-02 15:42:52 +0600

    server connected to mongodb

--- index.js
+++ index.js
@@ -6,14 +6,8 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 // middleware
-
 app.use(cors());
 app.use(express.json());
-
-
-// user: warehouseManager
-// pass: lgACvbD5qpikH3W3;
-
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dagwi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
