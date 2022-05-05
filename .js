69cd52913347f472c542d669ebfdf8ba5f32f565
SanyFaysal <tfoysalahmed@gmail.com>
2022-05-05 22:32:41 +0600

    delete and post api created

--- .js
+++ .js
@@ -1,77 +1,85 @@
 [
     {
         "name": "LG 80 Series 50 Alexa Built-in, 4K UHD Smart TV, 60Hz Refresh Rate, Filmmaker Mode, Game Optimizer (50UP8000, 2021)",
-        "quantity": 25000,
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing e"
+        "quantity": 100,
+        "price": 1200,
+        "supplier": "Walton",
+        "sold": "unsold",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing e",
+        "img": "https://m.media-amazon.com/images/I/81Nf+GR3mUL._AC_SY300_SX300_.jpg"
     },
     {
         "name": "LG 80 Series 50 Alexa Built-in, 4K UHD Smart TV, 60Hz Refresh Rate, Filmmaker Mode, Game Optimizer (50UP8000, 2021)",
-        "quantity": 25000,
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing e"
+        "quantity": 100,
+        "price": 1200,
+        "supplier": "Walton",
+        "sold": "unsold",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing e",
+        "img": "https://m.media-amazon.com/images/I/81Nf+GR3mUL._AC_SY300_SX300_.jpg"
     },
     {
         "name": "LG 80 Series 50 Alexa Built-in, 4K UHD Smart TV, 60Hz Refresh Rate, Filmmaker Mode, Game Optimizer (50UP8000, 2021)",
-        "quantity": 25000,
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing e"
+        "quantity": 100,
+        "price": 1200,
+        "supplier": "Walton",
+        "sold": "unsold",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing e",
+        "img": "https://m.media-amazon.com/images/I/81Nf+GR3mUL._AC_SY300_SX300_.jpg"
     },
     {
         "name": "LG 80 Series 50 Alexa Built-in, 4K UHD Smart TV, 60Hz Refresh Rate, Filmmaker Mode, Game Optimizer (50UP8000, 2021)",
-        "quantity": 25000,
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing e"
+        "quantity": 100,
+        "price": 1200,
+        "supplier": "Walton",
+        "sold": "unsold",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing e",
+        "img": "https://m.media-amazon.com/images/I/81Nf+GR3mUL._AC_SY300_SX300_.jpg"
     },
     {
         "name": "LG 80 Series 50 Alexa Built-in, 4K UHD Smart TV, 60Hz Refresh Rate, Filmmaker Mode, Game Optimizer (50UP8000, 2021)",
-        "quantity": 25000,
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing e"
+        "quantity": 100,
+        "price": 1200,
+        "supplier": "Walton",
+        "sold": "unsold",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing e",
+        "img": "https://m.media-amazon.com/images/I/81Nf+GR3mUL._AC_SY300_SX300_.jpg"
     },
     {
         "name": "LG 80 Series 50 Alexa Built-in, 4K UHD Smart TV, 60Hz Refresh Rate, Filmmaker Mode, Game Optimizer (50UP8000, 2021)",
-        "quantity": 25000,
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing e"
+        "quantity": 100,
+        "price": 1200,
+        "supplier": "Walton",
+        "sold": "unsold",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing e",
+        "img": "https://m.media-amazon.com/images/I/81Nf+GR3mUL._AC_SY300_SX300_.jpg"
     },
     {
         "name": "LG 80 Series 50 Alexa Built-in, 4K UHD Smart TV, 60Hz Refresh Rate, Filmmaker Mode, Game Optimizer (50UP8000, 2021)",
-        "quantity": 25000,
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing e"
+        "quantity": 100,
+        "price": 1200,
+        "supplier": "Walton",
+        "sold": "unsold",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing e",
+        "img": "https://m.media-amazon.com/images/I/81Nf+GR3mUL._AC_SY300_SX300_.jpg"
     },
     {
         "name": "LG 80 Series 50 Alexa Built-in, 4K UHD Smart TV, 60Hz Refresh Rate, Filmmaker Mode, Game Optimizer (50UP8000, 2021)",
-        "quantity": 25000,
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing e"
+        "quantity": 100,
+        "price": 1200,
+        "supplier": "Walton",
+        "sold": "unsold",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing e",
+        "img": "https://m.media-amazon.com/images/I/81Nf+GR3mUL._AC_SY300_SX300_.jpg"
     },
     {
         "name": "LG 80 Series 50 Alexa Built-in, 4K UHD Smart TV, 60Hz Refresh Rate, Filmmaker Mode, Game Optimizer (50UP8000, 2021)",
-        "quantity": 25000,
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing e"
-    },
-    {
-        "name": "LG 80 Series 50 Alexa Built-in, 4K UHD Smart TV, 60Hz Refresh Rate, Filmmaker Mode, Game Optimizer (50UP8000, 2021)",
-        "quantity": 25000,
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing e"
-    },
-    {
-        "name": "LG 80 Series 50 Alexa Built-in, 4K UHD Smart TV, 60Hz Refresh Rate, Filmmaker Mode, Game Optimizer (50UP8000, 2021)",
-        "quantity": 25000,
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing e"
-    },
-    {
-        "name": "LG 80 Series 50 Alexa Built-in, 4K UHD Smart TV, 60Hz Refresh Rate, Filmmaker Mode, Game Optimizer (50UP8000, 2021)",
-        "quantity": 25000,
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing e"
-    },
-    {
-        "name": "LG 80 Series 50 Alexa Built-in, 4K UHD Smart TV, 60Hz Refresh Rate, Filmmaker Mode, Game Optimizer (50UP8000, 2021)",
-        "quantity": 25000,
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing e"
-    },
-    {
-        "name": "LG 80 Series 50 Alexa Built-in, 4K UHD Smart TV, 60Hz Refresh Rate, Filmmaker Mode, Game Optimizer (50UP8000, 2021)",
-        "quantity": 25000,
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing e"
-    },
-    {
-        "name": "LG 80 Series 50 Alexa Built-in, 4K UHD Smart TV, 60Hz Refresh Rate, Filmmaker Mode, Game Optimizer (50UP8000, 2021)",
-        "quantity": 25000,
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing e"
+        "quantity": 100,
+        "price": 1200,
+        "supplier": "Walton",
+        "sold": "unsold",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing e",
+        "img": "https://m.media-amazon.com/images/I/81Nf+GR3mUL._AC_SY300_SX300_.jpg"
     }
+
+
 ]
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -12,21 +12,50 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dagwi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+console.log('db connected')
 async function run() {
     try {
+
         await client.connect();
         const warehouseCollection = client.db('warehouseManagement').collection('products');
 
-        app.get('/allProducts', async (req, res) => {
+        app.get('/products', async (req, res) => {
             const query = {};
             const cursor = warehouseCollection.find(query);
             const result = await cursor.toArray();
             res.send(result)
         })
-        app.get('/demoProducts', async (req, res) => {
-            const query = {};
-            const cursor = warehouseCollection.find(query).limit(6);
+        app.post('/products', async (req, res) => {
+            const query = req.body;
+            const result = await warehouseCollection.insertOne(query);
+            res.send(result)
+        })
+        app.get('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const cursor = warehouseCollection.find(query);
             const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        app.put('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const updateProduct = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    quantity: updateProduct.quantity,
+                },
+            };
+            const result = await warehouseCollection.updateOne(filter, updateDoc, options);
+            res.send(result)
+
+        })
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await warehouseCollection.deleteOne(query);
             res.send(result)
         })
     }
